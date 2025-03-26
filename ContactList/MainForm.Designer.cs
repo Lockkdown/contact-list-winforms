210fae12361d289301c936685ca744ddf03fac63
Ngo Xuan Hao <0706064@gmail.com>
2025-03-26 15:48:12 +0700

    Xóa Form thêm liên hệ

--- ContactList/MainForm.Designer.cs
+++ ContactList/MainForm.Designer.cs
@@ -43,13 +43,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(366, 185);
+            this.button1.Location = new System.Drawing.Point(336, 188);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(166, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Thêm liên hệ";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
